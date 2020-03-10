109e5e68483d77c4480395be7d6e65f2d388ed30
Fabien Berthomé <fberthome@ehess.fr>
2020-03-10 07:59:34 +0100

    Vigenere decrypt

--- exercices/exercice1-3-2/frequences.py
+++ exercices/exercice1-3-2/frequences.py
@@ -1,21 +1,25 @@
 WHITESPACE = " "
 
 
-def frequences(chaine):
+def frequences(chaine, size=1):
     freq = {};
-    for char in chaine:
-        if char != " ":
-            if char not in freq.keys():
-                freq[char] = 1;
+    vChaine = chaine.replace(" ", "");
+    for index in range(len(vChaine)):
+        charSeq = vChaine[index:index + size];
+        if charSeq != " ":
+            if charSeq not in freq.keys():
+                freq[charSeq] = 1;
             else:
-                freq[char] += 1;
+                freq[charSeq] += 1;
 
     return {k: v for k, v in sorted(freq.items(), key=lambda item: item[1], reverse=True)};
 
 
-def formarFrequences(freqCharDict):
+def formatFrequences(freqCharDict):
     return WHITESPACE.join("".join((str(k), str(v))) for k, v in freqCharDict.items());
 
 
 freqDict = frequences("Etre contesté, c’est être constaté");
-print(formarFrequences(freqDict));
+freqDict2 = frequences("Etre contesté, c’est être constaté", 2);
+print(formatFrequences(freqDict));
+print(formatFrequences(freqDict2));
