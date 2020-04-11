54f9be90749f724fc4b356756be9bcf85c069625
Fabien Berthomé <fberthome@ehess.fr>
2020-04-11 23:06:20 +0200

    naive proof of work

---
+++ exercices/exercice-1-4/naivepow.py
@@ -0,0 +1,26 @@
+import string
+import random
+import time
+import logging
+logging.basicConfig(level=logging.INFO)
+
+def randomString(length):
+    chaine = ""
+    for index in range(0, length):
+        chaine += random.choice(string.ascii_uppercase)
+    return chaine;
+
+def rechercheDebut(prefix, length):
+    start_time = time.time()
+    while True:
+        randomStr = randomString(length);
+        logging.debug(randomStr);
+        if randomStr.startswith(prefix):
+            break;
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+
+for longueur in range (8, 10):
+    print("longueur", longueur)
+    rechercheDebut("AAA", longueur)
+
