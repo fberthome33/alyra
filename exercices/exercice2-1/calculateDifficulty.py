723f7bbbe9c7b93bc63d9b6df90295526f9417e2
berthome.fabien@gmail.com <82xZrEgDfgBd>
2020-05-05 16:37:14 +0200

    Calculatrice Polonaise

--- exercices/exercice2-1/calculateDifficulty.py
+++ exercices/exercice2-1/calculateDifficulty.py
@@ -5,10 +5,14 @@ def cible(coefficient, exposant):
 
 
 def calculerDifficulteFromCible(cible):
-    return round(2.7 * 10**67 / cible, 1);
+    return 0xffff << 208 / cible;
+    #return 2.7 * 10**67 / cible;
 
 
 def calculerDifficulteFromBits(bits):
+    #return 0xffff / int(bits[4:10], 16) * 256 ** (29 - int(bits[2:4], 16))
+    if bits[0] == "0" and bits[1] == 'x':
+        bits = bits [2:]
     bits_byteArray = bytearray.fromhex(bits)
     exponent = int.from_bytes(bits_byteArray[0:1], byteorder='little');
     coeff = bits_byteArray[1:4];
@@ -17,3 +21,7 @@ def calculerDifficulteFromBits(bits):
 def blocReajustement(hauteurBloc):
     return hauteurBloc % 2016 == 0;
 
+print(calculerDifficulteFromCible(1147152896345386682952518188670047452875537662186691235300769792000))
+#23.50125722290317
+print(calculerDifficulteFromCible(1))
+#2.695953529101131e+67
--- exercices/exercice2-1/calculateDifficultyTest.py
+++ exercices/exercice2-1/calculateDifficultyTest.py
@@ -3,7 +3,9 @@ from calculateDifficulty import *
 
 class CalculateDifficultyTestCase(unittest.TestCase):
     def test_calculerDifficulteFromCible(self):
-        self.assertEqual(23.5, calculerDifficulteFromCible(1147152896345386682952518188670047452875537662186691235300769792000))
+        self.assertEqual(23.50125722290317, calculerDifficulteFromCible(
+            1147152896345386682952518188670047452875537662186691235300769792000))
+        self.assertEqual(2.695953529101131e+67, calculerDifficulteFromCible(1))
 
     def test_calculerDifficulteFromBits(self):
         self.assertEqual(23.5, calculerDifficulteFromBits("1c0ae493"))
