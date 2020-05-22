97241fab9ca17b635fe9f1da6902d48cbdf5f3e9
Fabien Berthomé <fberthome@ehess.fr>
2020-05-22 23:32:39 +0200

    Exercice 3.1.5 - Les administrateurs Celui qui déploie le smart contract est le premier administrateur. Il peut nommer un administrateur Un administrateur peut démissionner Une proposition de décision peut être fermée par un administrateur Un administrateur peut donner un blâme à un membre. Au deuxième blâme, celui-ci est expulsé. Pour transformer notre assemblée en démocratie, les administrateurs sont élus par une décision. Modifiez la structure Décision pour prendre en compte ce cas.

--- exercices/exercice3/assemblee.sol
+++ exercices/exercice3/assemblee.sol
@@ -1,11 +1,30 @@
 pragma solidity ^0.6.0;
 contract Assemblee {
+ struct Decision {
+   string description;
+   uint votesPour;
+   uint votesContre;
+
+   mapping (address => bool) aVote;
+   bool ouvert;
+   address administrateur;
+ }
+
+ mapping (address => int) blames;
  address[] membres;
- string[] descriptionDecisions;
- uint[] votesPour;
- uint[] votesContre;
+ address[] administrateurs;
+ Decision[] decisions;
+
+ string public nombAssemblee;
+
+ constructor(string memory nom) public {
+   nombAssemblee = nom;
+   administrateurs.push(msg.sender);
+ }
 
  function rejoindre() public {
+   require(estMembre(msg.sender) == false, "Vous êtes déjà membre");
+   require(blames[msg.sender] != 2, "Vous avez été expulsé de cette assemblée");
    membres.push(msg.sender);
  }
 
@@ -18,25 +37,87 @@ contract Assemblee {
     return false;
  }
 
+ function estAdministrateur() public view returns (bool) {
+    for (uint i=0; i<administrateurs.length; i++) {
+        if(administrateurs[i] == msg.sender) {
+            return true;
+        }
+    }
+    return false;
+ }
+
  function proposerDecision(string memory description) public {
-   if(estMembre(msg.sender)){
-     descriptionDecisions.push(description);
-     votesPour.push(0);
-     votesContre.push(0);
-   }
+   require(estMembre(msg.sender), "Il faut être membre!");
+   Decision memory decision;
+   decision.description = description;
+   decision.ouvert = true;
+   decisions.push(decision);
+ }
+
+  function proposerAdministrateur(string memory description, address  admninistrateur) public {
+   require(estMembre(msg.sender), "Il faut être membre!");
+   Decision memory decision;
+   decision.description = description;
+   decision.ouvert = true;
+   decision.administrateur = admninistrateur;
+   decisions.push(decision);
  }
 
  function voter(uint indice, bool value) public {
+    require(decisions[indice].aVote[msg.sender] == false, "Vous avez déjà voté pour cette decision");
+    require(decisions[indice].ouvert, "La decision n est pas ouverte au vote");
+    require(estMembre(msg.sender), "Il faut être membre!");
     if(value) {
-        votesPour[indice] += 1;
+        decisions[indice].votesPour++;
     } else {
-        votesContre[indice] += 1;
+        decisions[indice].votesContre++;
     }
+    decisions[indice].aVote[msg.sender] = true;
  }
 
  function comptabiliser(uint indice) public view returns (int){
-    return int(votesPour[indice] - votesContre[indice]);
+    return int(decisions[indice].votesPour - decisions[indice].votesContre);
  }
+
+ function nommerAdministrateur(address administrateur) public {
+    require(estAdministrateur(), "Vous devez être administrateur");
+    for (uint i=0; i < administrateurs.length; i++) {
+        if(administrateurs[i] == administrateur) {
+            revert();
+        }
+    }
+    administrateurs.push(administrateur);
+ }
+
+  function demissionAdministrateur() public {
+    require(estAdministrateur(), "Vous devez être administrateur");
+    for (uint i=0; i < administrateurs.length; i++) {
+        if(administrateurs[i] == msg.sender) {
+            delete administrateurs[i];
+        }
+    }
+ }
+
+   function fermerDecision(uint indice) public {
+    require(estAdministrateur(), "Vous devez être administrateur");
+    decisions[indice].ouvert = false;
+    if(decisions[indice].administrateur != address(0) &&  comptabiliser(indice) > 0) {
+        administrateurs.push(decisions[indice].administrateur);
+   }
+   }
+
+
+    function blame(address membre) public {
+        require(estAdministrateur(), "Vous devez être administrateur");
+        blames[membre]++;
+        if( blames[membre] == 2) {
+           for (uint i=0; i<membres.length; i++) {
+              if(membres[i] == membre) {
+                 delete membres[i];
+              }
+           }
+        }
+    }
 
 }
 
